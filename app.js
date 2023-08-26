89766dc3da477490176affc1fb6d79989f8c9afb
simbu karan <simbukaran390@gmail.com>
2023-08-26 13:14:58 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(
+  "mongodb+srv://admin-simbu:74628simbu@cluster0.ivcrsmp.mongodb.net/todolistDB"
+);git 
+
+const itemSchema = new mongoose.Schema({
+  itemName: String,
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const Item1 = new Item({
+  itemName: "Add your Works here.",
+});
+const Item2 = new Item({
+  itemName: "Hit the + to add new item",
+});
+const Item3 = new Item({
+  itemName: "<-- Hit this to delete  item",
+});
+
+const defaultItems = [Item1, Item2, Item3];
+
+const customListSchema = {
+  name: String,
+  items: [itemSchema],
+};
+
+async function insertItems() {
+  try {
+    const response = await Item.insertMany(defaultItems);
+    // console.log(response);
+    console.log("Items saved successfully");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+app.get("/", function (req, res) {
+  const day = new Date().toDateString();
+  async function fetchItems() {
+    try {
+      const foundItems = await Item.find();
+      //console.log(foundItems);
+      if (foundItems.length === 0) {
+        insertItems();
+        res.redirect("/");
+        return;
+      }
+      res.render("list.ejs", {
+        listTitle: day,
+        newlistItems: foundItems,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  fetchItems();
+});
+
+app.post("/", function (req, res) {
+  const createdItem = new Item({
+    itemName: req.body.newItem,
+  });
+  createdItem.save();
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const id = req.body.checkbox;
+  deleteItem();
+  res.redirect("/");
+  async function deleteItem() {
+    try {
+      const response = await Item.findByIdAndRemove(id);
+      if (response) {
+        console.log("Successfully deleted the item ");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
